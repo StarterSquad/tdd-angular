2cc4632e23af0a5189610e8befac0703f47891c4
Iwein Fuld <iwein@fuld.net>
2013-09-15 11:04:56 +0200

    Solves exercise: Mock the count function in the controller test

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -6,38 +6,31 @@ describe('CounterControl', function(){
 
   var scope, subject, countWordsOf;
 
+  var spies = {
+    count: function(string){}
+  };
+
   beforeEach(function(){
     module('myApp.controllers');
-    module('myApp.services');
+
+    module(function($provide){
+     $provide.value('count', spies.count)
+    });
+
+    spyOn(spies, 'count');
 
     inject(function($rootScope, $controller){
       scope = $rootScope.$new();
+
       subject = $controller('CounterControl', {$scope: scope});
       countWordsOf = scope.count;
     });
   });
 
-  //This is duplicated in the servicesSpec. Next exercise we'll take care of that.
   describe('word count', function (){
-
-    it('should count words in "foo bar"', function (){
-      expect(countWordsOf('foo bar')).toBe(2);
-    });
-
-    it('should count words in multiline string', function(){
-      expect(countWordsOf('foo bar\nbaz')).toBe(3);
-    });
-
-    it('should not count whitespace as words', function(){
-      expect(countWordsOf('foo     bar')).toBe(2);
-    });
-
-    it('should not count interpunction as words', function(){
-      expect(countWordsOf('foo - bar !')).toBe(2);
-    });
-
-    it('should deal with empty input', function (){
-      expect(countWordsOf(undefined)).toBe(0);
+    it('should use the service for counting', function (){
+      countWordsOf('foo bar');
+      expect(spies.count).toHaveBeenCalledWith('foo bar');
     });
   });
 });
